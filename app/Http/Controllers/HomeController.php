4bf2f17eb8959c030b8d877553a3c30716a39d17
Ha Manh Do <hwebz.com@gmail.com>
2020-08-31 10:46:58 -0700

    Searching videos and channels

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Channel;
+use App\Video;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,22 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $query = request()->search;
+
+        $videos = collect();
+
+        $channels = collect();
+
+        if ($query) {
+            // dd($query);
+
+            $videos = Video::where('title', 'LIKE', "%{$query}%")->orWhere('description', 'LIKE', "%{$query}%")->paginate(1);
+            $channels = Channel::where('name', 'LIKE', "%{$query}%")->orWhere('description', 'LIKE', "%{$query}%")->paginate(1, ['*'], 'channel_page');
+        }
+
+        return view('home')->with([
+            'videos' => $videos,
+            'channels' => $channels
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,9 +14,71 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <form acton="">
+                        <input type="text" class="form-control" name="search" placeholder="Search videos and channels..." />
+                    </form>
                 </div>
             </div>
+
+            @if($channels->count() > 0)
+                <div class="card mt-3">
+                    <div class="card-header">Channels</div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($channels as $channel)
+                                    <tr>
+                                        <td>{{ $channel->name }}</td>
+                                        <td>
+                                            <a href="{{ route('channels.show', $channel->id)}}" class="btn btn-info">View</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        <div class="row justify-content-center">
+                            {{ $channels->appends(request()->query())->links() }}
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+            @if($videos->count() > 0)
+                <div class="card mt-3">
+                    <div class="card-header">Videos</div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($videos as $video)
+                                    <tr>
+                                        <td>{{ $video->title }}</td>
+                                        <td>
+                                            <a href="{{ route('videos.show', $video->id)}}" class="btn btn-info">View</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        <div class="row justify-content-center">
+                            {{ $videos->appends(request()->query())->links() }}
+                        </div>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </div>
